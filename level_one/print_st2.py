6708dbeee20a61a2f490cb98754f640568d57aca
emmystringzhs <milesbetti@gmail.com>
2024-04-17 14:46:15 +0100

    this is just dealing with integer an str.format type printing

---
+++ level_one/print_st2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+The output of the script should be:
+the number, followed by Battery street,
+followed by a new line
+"""
+
+number = 98
+print("{}, Battery street".format(number))
