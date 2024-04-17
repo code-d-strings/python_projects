40b4d7407939fe4bf45243580cb1630afd5e20f6
emmystringzhs <milesbetti@gmail.com>
2024-04-17 15:11:44 +0100

    this is just dealing with integer an float type printing

---
+++ level_one/print_st3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+The output of the program should be:
+Float:, followed by the float with only 2 digits
+followed by a new line
+"""
+number = 3.14159
+
+print(f"Float: {number: .2f}")
